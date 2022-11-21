1e01da9be8065c3c60a40a021e1be82257d2e276
Supachai Wongmoon <supachai@outlook.com>
2022-11-21 20:41:46 +0700

    Add router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import HomePage from '@/components/HomePage.vue'
+import ContactPage from '@/components/ContactPage.vue'
+
+const routes = [
+  {path: '/', component: HomePage},
+  {path: '/contact', component: ContactPage},
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+createApp(App).use(router).mount('#app')
